b025de6cf406cc4d9dfec2680c879c3a56ab57b3
Sevdi <sevdicek7260@gmail.com>
2026-05-30 06:52:31 +0300

    update

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async';
+import { Toaster } from 'react-hot-toast';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import Admin from './pages/Admin';
+
+function App() {
+  return (
+    <HelmetProvider>
+      <Router>
+        <div className="flex" style={{ flexDirection: 'column', minHeight: '100vh' }}>
+          <Header />
+          <main style={{ flex: 1 }}>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/admin" element={<Admin />} />
+            </Routes>
+          </main>
+          <Footer />
+        </div>
+      </Router>
+      <Toaster position="top-right" toastOptions={{ duration: 3000, style: { background: '#333', color: '#fff', borderRadius: '10px' } }} />
+    </HelmetProvider>
+  );
+}
+
+export default App;
